eb4104142d84bee98a345dd24da4e72d94b884bc
Emi Hoess <emihoess@gmail.com>
2019-05-13 18:15:41 -0300

    GDT & Protected mode
    https://en.wikipedia.org/wiki/Protected_mode

--- src/gdt.c
+++ src/gdt.c
@@ -2,7 +2,7 @@
 ================================================================================
  TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
 ================================================================================
-  definicion de la tabla de descriptores globales
+Global descritors table
 */
 #include "defines.h"
 #include "gdt.h"
@@ -27,17 +27,110 @@ gdt_entry gdt[GDT_COUNT] = {
 		(unsigned char)		0x00,			/* base[31:24]  */
 	},
 
+	//Code 1, privilegio 0
 	[1] = {
+		//Limit: máximo offset que puede tener un byte en un segmento
+		.limit_0_15  = 0xFFFF,
+		//Base: dirección a partir de la cuál se tiene el segmento
+		.base_0_15   = 0,
+		.base_23_16  = 0,
+		.type        = 10, //101
+		//System: s = 0 => recurso del kernel, s=1 => codigo o datos
+		.s           = 1,
+		//Descriptor de privilegios.
+		.dpl         = 0,
+		//Present: p = 1 => segmento presente en la ram
+		.p           = 1,
+		.limit_16_19 = 0x7,
+		//Available: bit sin uso
+		.avl         = 0,
+		//L siempre 0, => 32 bits, L=1 y db = 0 => 64 bits de segmento
+		.l           = 0,
+		//Default Big: configura el tamaño del segmento, si db = 1, 32 bits
+		//si db = 0, 16 bits
+		.db          = 1,
+		//Granularidad: establece la unidad de medida de "límite",
+		//Si g = 1, el límite es multiplicado por 4096
+		//Si g = 0, el límite es tal cuál está
+		.g           = 1,
+		.base_31_24  = 0,
+	},
+	//Code 2, privilegio 2
+	[2] = {
 		.limit_0_15  = 0xFFFF,
 		.base_0_15   = 0,
 		.base_23_16  = 0,
-		.type        = ,
-		.s           = ,
-		.dpl         = ,
+		.type        = 10,
+		.s           = 1,
+		.dpl         = 2,
 		.p           = 1,
 		.limit_16_19 = 0x7,
 		.avl         = 0,
-		.l           = ,
+		.l           = 0,
+		.db          = 1,
+		.g           = 1,
+		.base_31_24  = 0,
+	},
+	//Code 3, privilegio 3
+	[3] = {
+		.limit_0_15  = 0xFFFF,
+		.base_0_15   = 0,
+		.base_23_16  = 0,
+		.type        = 10,
+		.s           = 1,
+		.dpl         = 3,
+		.p           = 1,
+		.limit_16_19 = 0x7,
+		.avl         = 0,
+		.l           = 0,
+		.db          = 1,
+		.g           = 1,
+		.base_31_24  = 0,
+	},
+	//Data 1, privilegio 0
+	[4] = {
+		.limit_0_15  = 0xFFFF,
+		.base_0_15   = 0,
+		.base_23_16  = 0,
+		.type        = 2, //0010
+		.s           = 1,
+		.dpl         = 0,
+		.p           = 1,
+		.limit_16_19 = 0x7,
+		.avl         = 0,
+		.l           = 0,
+		.db          = 1,
+		.g           = 1,
+		.base_31_24  = 0,
+	},
+	//Data 2, privilegio 2
+	[5] = {
+		.limit_0_15  = 0xFFFF,
+		.base_0_15   = 0,
+		.base_23_16  = 0,
+		.type        = 2,
+		.s           = 1,
+		.dpl         = 2,
+		.p           = 1,
+		.limit_16_19 = 0x7,
+		.avl         = 0,
+		.l           = 0,
+		.db          = 1,
+		.g           = 1,
+		.base_31_24  = 0,
+	},
+	//Data 3, privilegio 3
+	[6] = {
+		.limit_0_15  = 0xFFFF,
+		.base_0_15   = 0,
+		.base_23_16  = 0,
+		.type        = 2,
+		.s           = 1,
+		.dpl         = 3,
+		.p           = 1,
+		.limit_16_19 = 0x7,
+		.avl         = 0,
+		.l           = 0,
 		.db          = 1,
 		.g           = 1,
 		.base_31_24  = 0,
--- src/kernel.asm
+++ src/kernel.asm
@@ -50,14 +50,18 @@ start:
 	; habilitar A20
 	call habilitar_A20
 
-	; dehsabilitar las interrupciones
-
 	; cargar la GDT
 
+	lgdt [GDT_DESC]
 	; setear el bit PE del registro CR0
-
+	xchg bx, bx
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
 	; pasar a modo protegido
-
+	jmp 0x8:modo_protegido
+	BITS 32
+	modo_protegido:
 	; acomodar los segmentos
 
 	; seteo la pila
